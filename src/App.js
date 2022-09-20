b40b15e45c60b07605c9599201e87e8e6ba7afd8
Suzon999 <ashikurrahamansuzon346112.com>
2022-09-20 20:06:05 +0600

    daynamic data map

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@
 
 import './App.css';
 
-
+const nayoks = [
+  { name: ' Dr. Mahafuz', job: 'singer' },
+  { name: ' sakib khan', job: 'nayok' },
+  { name: ' Ruble', job: 'hero' },
+  { name: ' josim', job: 'moskai na' }
+]
 
 
 function App() {
   return (
     <div className="App">
-      <Person name='Sakib khan' naiyka='Aopu bissah'></Person>
+      {
+        nayoks.map(nayok => <Person names={nayok.name} job={nayok.job} ></Person>)
+      }
+      {/* <Person name='Sakib khan' naiyka='Aopu bissah'></Person>
       <Person name='BappaRazz' naiyka='Shaka'></Person>
-      <Person name='Ruble' naiyka='Mousumi'></Person>
+      <Person name='Ruble' naiyka='Mousumi'></Person> */}
 
 
     </div>
@@ -20,8 +28,8 @@ function Person(props) {
   console.log(props);
   return (
     <div className='person'>
-      <h1>{props.name} </h1>
-      <h3>{props.naiyka} </h3>
+      <h1>{props.names} </h1>
+      <h3>{props.job} </h3>
     </div>
   )
 }

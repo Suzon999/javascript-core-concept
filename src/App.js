5e659d0de616f9dbf4685c52e8dd340b290448a4
Suzon999 <ashikurrahamansuzon346112.com>
2022-09-20 19:36:57 +0600

    js corwe concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name='Sakib khan' naiyka='Aopu bissah'></Person>
+      <Person name='BappaRazz' naiyka='Shaka'></Person>
+      <Person name='Ruble' naiyka='Mousumi'></Person>
+
+
     </div>
   );
+}
+function Person(props) {
+  console.log(props);
+  return (
+    <div className='person'>
+      <h1>{props.name} </h1>
+      <h3>{props.naiyka} </h3>
+    </div>
+  )
 }
 
 export default App;
